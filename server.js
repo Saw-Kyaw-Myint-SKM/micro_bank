804f13e245350ef5189da155d87a910952d043f6
Saw Kyaw Myint <scm.sawkyawmyint@gmail.com>
2024-07-22 20:32:18 +0630

    add socket io

---
+++ server.js
@@ -0,0 +1,27 @@
+import express from "express";
+import http from "http";
+import { Server } from "socket.io";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const server = http.createServer(app);
+
+const io = new Server(server, {
+    cors: { origin: "*" },
+});
+
+io.on("connection", (socket) => {
+    socket.on("updateTransition", (message) => {
+        if (message === "reloadTransfer") {
+            socket.broadcast.emit("updateTransition");
+        }
+    });
+});
+
+const port = process.env.VITE_SOCKET_IO_PORT || 3000;
+server.listen(port, () => {
+    // To know server is running or not
+    console.log("Server is running. Port: " + port);
+});
